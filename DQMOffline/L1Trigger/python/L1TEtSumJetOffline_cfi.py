d024bc3d95229c3a73b3993512c39e3daf235a13
Ben Krikler <b.krikler@cern.ch>
2019-03-11 12:36:58 +0100

    Start adding a prefiring matching set of plots to the DQMOffline

--- DQMOffline/L1Trigger/python/L1TEtSumJetOffline_cfi.py
+++ DQMOffline/L1Trigger/python/L1TEtSumJetOffline_cfi.py
@@ -146,6 +146,13 @@ l1tEtSumJetOfflineDQM = DQMEDAnalyzer(
     ),
 )
 
+l1tPrefiringOfflineDQM = DQMEDAnalyzer(
+      "L1TPrefiringDQMOffline",
+      tcdsRecordLabel = cms.InputTag("tcdsDigis","tcdsRecord"),
+      offlineJetSource = cms.InputTag("goodPFJetsForL1T"),
+      stage2CaloLayer2JetSource=cms.InputTag("caloStage2Digis", "Jet")
+)
+
 # modifications for the pp reference run
 jetEfficiencyThresholds_HI = [8, 16, 24, 44, 60, 80, 90]
 jetEfficiencyBins_HI = []
@@ -179,6 +186,7 @@ l1tEtSumJetOfflineDQMSeq = cms.Sequence(
     goodPFJetsForL1T
     + l1tPFMetNoMuForDQM
     + l1tEtSumJetOfflineDQM
+    + l1tPrefiringOfflineDQM
 )
 
 l1tEtSumJetOfflineDQMEmuSeq = cms.Sequence(
